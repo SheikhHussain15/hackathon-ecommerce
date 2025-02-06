015285df0e0904531235d809815363da94cbe4d8
Hussain Raza <hr7730774@gmail.com>
2025-02-06 22:43:58 +0500

    first commit

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -1,38 +1,38 @@
-import { Car } from "../../../types/cars"
+import { Car } from "../../../types/cars";
 
-export const addToCart = (car : Car) => {
-    const cart : Car[] = JSON.parse(localStorage.getItem('cart') || '[]')
+export const addToCart = (car: Car) => {
+    const cart: Car[] = JSON.parse(localStorage.getItem("cart") || "[]");
 
-    const existingCarIndex = cart.findIndex(item => item._id === car._id)
+    const existingCarIndex = cart.findIndex((item) => item._id === car._id);
 
-    if(existingCarIndex > -1 ) {
-        cart[existingCarIndex].inventory += 1
-    }
-    else {
-        cart.push({
-            ...car, inventory : 1
-        })
+    if (existingCarIndex > -1) {
+        cart[existingCarIndex].inventory += 1;
+    } else {
+        cart.push({ ...car, inventory: 1 });
     }
 
-    localStorage.setItem('cart', JSON.stringify(cart))
-}
+    localStorage.setItem("cart", JSON.stringify(cart));
+};
 
-export const removeFromCart = (carId : number) => {
-    let cart : Car[] = JSON.parse(localStorage.getItem('cart') || '[]')
-    cart = cart.filter(item => item._id !== carId)
-    localStorage.setItem('cart', JSON.stringify(cart))
-}
+export const removeFromCart = (carId: number) => {
+    let cart: Car[] = JSON.parse(localStorage.getItem("cart") || "[]");
+    cart = cart.filter((item) => item._id !== carId);
+    localStorage.setItem("cart", JSON.stringify(cart));
+};
 
-export const updateCartQuantity = (carId :number, quantity : number) => {
-    const cart : Car[] = JSON.parse(localStorage.getItem('cart') || '[]')
-    const carIndex = cart.findIndex(item => item._id === carId)
+export const updateCartQuantity = (carId: number, quantity: number) => {
+    const cart: Car[] = JSON.parse(localStorage.getItem("cart") || "[]");
+    const carIndex = cart.findIndex((item) => item._id === carId);
 
-    if(carIndex > -1) {
+    if (carIndex > -1) {
         cart[carIndex].inventory = quantity;
-        localStorage.setItem('cart', JSON.stringify(cart))
+        localStorage.setItem("cart", JSON.stringify(cart));
     }
-}
+};
 
-export const getCartItems = () : Car[] => {
-    return JSON.parse(localStorage.getItem('cart') || '[]')
-}
+export const getCartItems = (): Car[] => {
+    if (typeof window !== "undefined") {
+        return JSON.parse(localStorage.getItem("cart") || "[]");
+    }
+    return [];
+};
